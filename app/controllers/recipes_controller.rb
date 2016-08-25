9d55091600338a59690dc0b6dbe350e0aee8748d
Michael Maramag <charles.chryzten@gmail.com>
2016-08-25 18:24:20 +0800

    Finish Styling

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,6 @@
 class RecipesController < ApplicationController
 	before_action :find_recipe, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!, except: [:index, :show]
 
 	def index
 		@recipe = Recipe.all.order("created_at DESC")
@@ -9,15 +10,16 @@ class RecipesController < ApplicationController
 	end
 
 	def new
-		@recipe = Recipe.new
+		@recipe = current_user.recipes.build
 	end 
 
 	def create
-		@recipe = Recipe.new(recipe_params)
-
+		@recipe = current_user.recipes.build(recipe_params)
 		if @recipe.save
-			redirect_to @recipe, notice: "Your recipe was successfully created!"
+			flash[:success] = "Your recipe was successfully created!"
+			redirect_to @recipe
 		else
+			flash[:danger] = "New recipe failed to save, Try again!"
 			render 'new'
 		end
 	end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,10 +1,12 @@
 class Recipe < ApplicationRecord
+	belongs_to :user
 
-	has_many :ingredients
-	has_many :directions
+	has_many :ingredients, inverse_of: :recipe
+	has_many :directions, inverse_of: :recipe
 	
-	accepts_nested_attributes_for :ingredients, reject_if: proc { |attributes| attributes['name'].blank? }, allow_destroy: true
-	accepts_nested_attributes_for :directions, reject_if: proc { |attributes| attributes['step'].blank? }, allow_destroy: true
+
+  accepts_nested_attributes_for :ingredients, reject_if: :all_blank, allow_destroy: true
+ 	accepts_nested_attributes_for :directions, reject_if: :all_blank, allow_destroy: true
 	
 	validates :title, :description, :image, presence: true
   
